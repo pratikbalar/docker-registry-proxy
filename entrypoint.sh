5610cc8b143fd34aff3fd2a6440f36543127b74d
Pratik Balar <pratikbalar123@gmail.com>
2022-01-01 15:20:03 +0530

    refact(cacert): multiline rest openssl

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,10 +16,6 @@ logErr() {
 function creatCa() {
     declare -i DEBUG=0
 
-    logInfo() {
-        echo "INFO: $*"
-    }
-
     PROJ_NAME=DockerMirrorBox
     logInfo "Will create certificate with names ${1}"
 
@@ -87,8 +83,15 @@ EOF
     openssl genrsa -des3 -passout pass:foobar -out ia.key 4096 &>/dev/null
 
     logInfo "Create a signing request for the IA: ${CAID}"
-    openssl req -new -key ia.key -out ia.csr -passin pass:foobar -subj "/C=NL/ST=Noord Holland/L=Amsterdam/O=ME/OU=IT/CN=${CN_IA}" -reqexts IA -config <(
-        cat <<-EOF
+    openssl req \
+        -new \
+        -key ia.key \
+        -out ia.csr \
+        -passin pass:foobar \
+        -subj "/C=NL/ST=Noord Holland/L=Amsterdam/O=ME/OU=IT/CN=${CN_IA}" \
+        -reqexts IA \
+        -config <(
+            cat <<-EOF
 [req]
 distinguished_name = dn
 [dn]
@@ -97,7 +100,7 @@ basicConstraints = critical,CA:TRUE,pathlen:0
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
 subjectKeyIdentifier = hash
 EOF
-    )
+        )
 
     if [[ "${DEBUG}" ]]; then
         logInfo "Show the singing request, to make sure extensions are there"
@@ -105,8 +108,18 @@ EOF
     fi
 
     logInfo "Sign the IA request with the CA cert and key, producing the IA cert"
-    openssl x509 -req -days 730 -in ia.csr -CA "${CA_CRT_FILE}" -CAkey "${CA_KEY_FILE}" -CAserial "${CA_SRL_FILE}" -out ia.crt -passin pass:foobar -extensions IA -extfile <(
-        cat <<-EOF
+    openssl x509 \
+        -req \
+        -days 730 \
+        -in ia.csr \
+        -CA "${CA_CRT_FILE}" \
+        -CAkey "${CA_KEY_FILE}" \
+        -CAserial "${CA_SRL_FILE}" \
+        -out ia.crt \
+        -passin pass:foobar \
+        -extensions IA \
+        -extfile <(
+            cat <<-EOF
 [req]
 distinguished_name = dn
 [dn]
@@ -115,7 +128,7 @@ basicConstraints = critical,CA:TRUE,pathlen:0
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
 subjectKeyIdentifier = hash
 EOF
-    ) &>/dev/null
+        ) &>/dev/null
 
     if [[ "${DEBUG}" ]]; then
         logInfo "show the IA cert details"
@@ -130,7 +143,15 @@ EOF
     openssl rsa -passin pass:foobar -in web.orig.key -out web.key &>/dev/null
 
     logInfo "Create the signing request, using extensions"
-    openssl req -new -key web.key -sha256 -out web.csr -passin pass:foobar -subj "/C=NL/ST=Noord Holland/L=Amsterdam/O=ME/OU=IT/CN=${CN_WEB}" -reqexts SAN -config <(printf '[req]\ndistinguished_name = dn\n[dn]\n[SAN]\nsubjectAltName=%s' "${1}")
+    openssl req \
+        -new \
+        -key web.key \
+        -sha256 \
+        -out web.csr \
+        -passin pass:foobar \
+        -subj "/C=NL/ST=Noord Holland/L=Amsterdam/O=ME/OU=IT/CN=${CN_WEB}" \
+        -reqexts SAN \
+        -config <(printf '[req]\ndistinguished_name = dn\n[dn]\n[SAN]\nsubjectAltName=%s' "${1}")
 
     if [[ "${DEBUG}" ]]; then
         logInfo "Show the singing request, to make sure extensions are there"
@@ -138,7 +159,16 @@ EOF
     fi
 
     logInfo "Sign the request, using the intermediate cert and key"
-    openssl x509 -req -days 365 -in web.csr -CA ia.crt -CAkey ia.key -out web.crt -passin pass:foobar -extensions SAN -extfile <(printf '[req]\ndistinguished_name = dn\n[dn]\n[SAN]\nsubjectAltName=%s' "${1}") &>/dev/null
+    openssl x509 \
+        -req \
+        -days 365 \
+        -in web.csr \
+        -CA ia.crt \
+        -CAkey ia.key \
+        -out web.crt \
+        -passin pass:foobar \
+        -extensions SAN \
+        -extfile <(printf '[req]\ndistinguished_name = dn\n[dn]\n[SAN]\nsubjectAltName=%s' "${1}") &>/dev/null
 
     if [[ "${DEBUG}" ]]; then
         logInfo "Show the final cert details"
@@ -324,7 +354,7 @@ fi
 # normally use non-debug version of nginx
 NGINX_BIN="/usr/sbin/nginx"
 
-if [[ "${DEBUG}" == true ]]; then
+if [[ "${DEBUG}" ]]; then
     if [[ ! -f /usr/bin/mitmweb ]]; then
         logErr "To debug, you need the -debug version of this image, eg: :latest-debug"
         exit 3
